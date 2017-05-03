6076642e4b34b0dbf1fb1b5050a5fb7d4925749b
Elen Niedermeyer <elen.niedermeyer@posteo.de>
2017-05-03 17:03:23 +0200

    Code documentation

--- app/src/main/java/niedermeyer/nonogram/GameHandler.java
+++ app/src/main/java/niedermeyer/nonogram/GameHandler.java
@@ -4,13 +4,9 @@ import android.app.Activity;
 import android.os.Handler;
 import android.os.Message;
 import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.Space;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.Map;
@@ -19,7 +15,7 @@ import niedermeyer.nonogram.logics.NonogramFields;
 import niedermeyer.nonogram.logics.NonogramGenerator;
 
 /**
- * @author Elen Niedermeyer, last updated 2017-04-28
+ * @author Elen Niedermeyer, last updated 2017-05-02
  */
 
 public class GameHandler extends Handler {
@@ -33,39 +29,64 @@ public class GameHandler extends Handler {
 
     private int[][] actualField;
 
+    /**
+     * Constructor.
+     *
+     * @param pActivity the activity in which the game handler is used
+     */
     public GameHandler(Activity pActivity) {
         activity = pActivity;
     }
 
+    /**
+     * Overrides the method handleMessage in the class Handler.
+     * Creates a new game field with the size which is given by the message.
+     *
+     * @param msg
+     */
     @Override
     public void handleMessage(Message msg) {
+        // get message object
         int[] numbers = (int[]) msg.obj;
         int numberOfRows = numbers[0];
         int numberOfColumns = numbers[1];
 
+        // make new game field and initialize the private fields
         generator.makeNewGame(numberOfRows, numberOfColumns);
         nonogram = generator.getNonogram();
         rowCounts = generator.getCountsRow();
         columnCounts = generator.getCountsColumn();
 
+        // make a new array for the actual game field
         actualField = new int[numberOfRows][numberOfColumns];
 
         generateGameField();
     }
 
+    /**
+     * Makes the game field in the layout.
+     * Makes a table row for each row. IDs are the place in the nonogram array.
+     * Adds the counts left for the rows and on the top for the columns.
+     */
     private void generateGameField() {
         TableLayout table = (TableLayout) activity.findViewById(R.id.game_table);
 
         TableRow.LayoutParams rowParams = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT);
 
+        // add row with counts of the columns
+        TableRow columnCounts = makeColumnCountRow();
+        columnCounts.setLayoutParams(rowParams);
+        table.addView(columnCounts);
+
+        // add rows of nonogram
         for (int i = 0; i < nonogram.length; i++) {
             // new row
             TableRow row = new TableRow(activity);
             row.setLayoutParams(rowParams);
 
             // add count for this row
-            row.addView(addRowCount(i));
-            // add a button for each field
+            row.addView(makeRowCountView(i));
+            // add a button for each field in this row
             for (int j = 0; j < nonogram[i].length; j++) {
                 Button b = new Button(activity);
                 // make and set id
@@ -86,14 +107,54 @@ public class GameHandler extends Handler {
         }
     }
 
-    private TextView addRowCount(int i) {
-        ArrayList<Integer> values = rowCounts.get(i);
-        String countsAsText = "";
-        for (int value : values) {
-            countsAsText += value + " ";
+    /**
+     * Makes a table column that shows the column counts in text views.
+     * Numbers in one text view are separated by line separator.
+     *
+     * @return a table row with column counts
+     */
+    private TableRow makeColumnCountRow() {
+        TableRow row = new TableRow(activity);
+
+        // add an empty text view
+        row.addView(new TextView(activity));
+
+        // add filled views for all columns
+        for (int i = 0; i < columnCounts.size(); i++) {
+            ArrayList<Integer> values = columnCounts.get(i);
+            String countsAsText = "";
+            // paste all values for one column to one string
+            for (int value : values) {
+                countsAsText += "\n" + value;
+            }
+
+            // makes the new text view with the pasted text
+            TextView counts = new TextView(activity, null, R.style.CountViews);
+            counts.setText(countsAsText);
+
+            // add the text view to the row
+            row.addView(counts);
         }
 
-        TextView counts = new TextView(activity);
+        return row;
+    }
+
+    /**
+     * Makes a text view with the row count of row i. Numbers in the text view are separated by tabs.
+     *
+     * @param i the number of the row, which count should be in the text view
+     * @return a text view with the row counts of row i
+     */
+    private TextView makeRowCountView(int i) {
+        ArrayList<Integer> values = rowCounts.get(i);
+        String countsAsText = "";
+        // paste all numbers for this row to one string
+        for (int value : values) {
+            countsAsText += "\t" + value;
+        }
+
+        // makes the new text view with the pasted string
+        TextView counts = new TextView(activity, null, R.style.CountViews);
         counts.setText(countsAsText);
 
         return counts;
