17cd20688509827552472f3f6b295cbd460e3dc1
Elen Niedermeyer <elen.niedermeyer@posteo.de>
2017-08-10 16:32:08 +0200

    Add a skip button to the How to play activity

--- app/src/main/java/niedermeyer/nonogram/activities/HowToPlayActivity.java
+++ app/src/main/java/niedermeyer/nonogram/activities/HowToPlayActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TextView;
@@ -14,14 +15,14 @@ import android.widget.TextView;
 import niedermeyer.nonogram.R;
 
 /**
- * @author Elen Niedermeyer, last modified 2017-08-09
+ * @author Elen Niedermeyer, last modified 2017-08-10
  */
 public class HowToPlayActivity extends AppCompatActivity implements OnClickListener {
 
     /**
-     * Constant for the position of the table, second element in root layout
+     * Constant for the position of the table, third element in root layout
      */
-    private static final int INDEX_POSITION_TABLE = 1;
+    private static final int INDEX_POSITION_TABLE = 2;
 
     /**
      * Views
@@ -29,6 +30,7 @@ public class HowToPlayActivity extends AppCompatActivity implements OnClickListe
     private LinearLayout root;
     private TextView instruction;
     private TextView clickText;
+    private Button skip;
 
     /**
      * Index for the arrays {@link #instructions} and {@link #tables}.
@@ -68,16 +70,13 @@ public class HowToPlayActivity extends AppCompatActivity implements OnClickListe
                 clickText.setText(getString(R.string.click_here_new_game));
             }
         } else {
-            // it's the end of the array, start game
-            index = 0;
-            Intent i = new Intent(this, NonogramActivity.class);
-            startActivity(i);
+            changeToNonogramActivity();
         }
     }
 
     /**
      * Overrides the method {@link AppCompatActivity#onCreate(Bundle, PersistableBundle)}.
-     * Sets the layout. Iitializes the views {@link #root}, {@link #instruction} and {@link #clickText}.
+     * Sets the layout. Initializes the views {@link #root}, {@link #instruction}, {@link #clickText} and {@link #skip}. Sets an on click listener for {@link #skip}.
      * Initializes the arrays {@link #instructions} and {@link #tables}. Sets the first instruction pair of string and table.
      * Sets the on click listener at the root view.
      *
@@ -91,6 +90,15 @@ public class HowToPlayActivity extends AppCompatActivity implements OnClickListe
         root = (LinearLayout) findViewById(R.id.activity_how_to_play_root);
         instruction = (TextView) findViewById(R.id.activity_how_to_play_description);
         clickText = (TextView) findViewById(R.id.activity_how_to_play_text_click);
+
+        skip = (Button) findViewById(R.id.activity_how_to_play_btn_skip);
+        // start the nonogram activity, when the skip button was clicked
+        skip.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeToNonogramActivity();
+            }
+        });
 
         index = 0;
         // load strings
@@ -113,5 +121,15 @@ public class HowToPlayActivity extends AppCompatActivity implements OnClickListe
 
         // adds the on click listener
         root.setOnClickListener(this);
+    }
+
+    /**
+     * Sets the index to zero.
+     * Start the nonogram activity.
+     */
+    private void changeToNonogramActivity() {
+        // it's the end of the array, start game
+        index = 0;
+        startActivity(new Intent(this, NonogramActivity.class));
     }
 }
